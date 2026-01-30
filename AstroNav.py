4b89226547555c05d612247b14b9e09e5b737ddc
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2026-01-30 17:12:03 -0600

    Add AstroNav.py for displaying a star universe

---
+++ AstroNav.py
@@ -0,0 +1,57 @@
+from microbit import *
+import random
+import time
+D=40
+
+def mkUni(diam):
+    for i in range(3):
+        display.show(Image.DIAMOND_SMALL)
+        time.sleep(.2)
+        display.show(Image.DIAMOND)
+        time.sleep(.2)
+        
+    Universe = []
+    for i in range(diam*diam):
+        if random.randrange(10)<6: #star
+            Universe.append(1+random.randrange(7))
+        else:
+            Universe.append(0)
+    return Universe
+
+def show(x,y,uni):
+    for yy in range(5):
+        for xx in range(5):
+            display.set_pixel(xx,yy,uni[xx+x+y*D+yy])
+
+x = 0
+y = 0
+cosmos = mkUni(D)
+
+show(0,0,cosmos)
+
+while True:
+    if pin_logo.is_touched():
+        cosmos = mkUni(D)
+        x=0
+        y=0
+        show(x,y,cosmos)
+    if accelerometer.was_gesture("left"):
+        x = x - 1
+        if x<0:
+            x=0
+        show(x,y,cosmos)
+    if accelerometer.was_gesture("right"):
+        x = x + 1
+        if x>D-4:
+            x=D-4
+        show(x,y,cosmos)
+    if accelerometer.was_gesture("up"):
+        y = y - 1
+        if y<0:
+            y=0
+        show(x,y,cosmos)
+    if accelerometer.was_gesture("down"):
+        y = y + 1
+        if y>D-4:
+            y=D-4
+        show(x,y,cosmos)
